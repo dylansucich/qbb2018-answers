7eb25671a516f3314d0f862fc6de8ddce40c834f
dylansucich <dgsucich@gmail.com>
2018-08-28 14:18:18 -0400

    Correct doRNA-seq.sh

--- day1-homework/doRNA-seq.sh
+++ day1-homework/doRNA-seq.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-GENOME=../genomes/BDGP6
-GUIDE=../genomes/BDGP6.Ensembl.81.gtf
-SAMPLE=~/Users/cmdb/data/rawdata/
+GENOME="/Users/cmdb/qbb2018-answers/genomes/BDGP6"
+ANNOTATION="/Users/cmdb/qbb2018-answers/genomes/BDGP6.Ensembl.81.gtf"
 
-for * in $SAMPLE 
+
+for SAMPLE in  SRR072893 SRR072903 SRR072905 SRR072915
 do
-  mkdir ~/Users/cmdb/qbb2018-answers/day1-homework/$SAMPLE
-  fastqc ~/Users/cmdb/qbb2018-answers/day1-homework/$SAMPLE.fastq
-  
+  mkdir $SAMPLE
+  cd $SAMPLE
+  fastqc ../${SAMPLE}.fastq
+  hisat2 -p 8 -x ${GENOME} -U ../${SAMPLE}.fastq -S ${SAMPLE}.sam
+  samtools view ${SAMPLE}.sam -b -o ${SAMPLE}.bam
+  samtools sort -O BAM -o ${SAMPLE}.sorted.bam ${SAMPLE}.bam
+  samtools index ${SAMPLE}.sorted.bam
+  stringtie -p 8 -e ${SAMPLE}.sorted.bam -G $ANNOTATION -o ${SAMPLE}_stringtie.gtf -B
+  cd ../
 done
-
-#hisat2 -p 8 -x BDGP6 -U ../day1-homework/m10. -S m10.sam 
