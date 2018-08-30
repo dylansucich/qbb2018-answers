96f451ed182842a30ddb607bbb6ddd155caa7cad
dylansucich <dgsucich@gmail.com>
2018-08-30 14:49:48 -0400

    Day4-lunch exercise 2: Create scatterplot of FPKMs from two <ctab> files. Python script and scatterplot output

---
+++ day4-lunch/day4-exercise2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+#/data/results/stringtie/SRR072893/t_data.ctab
+"""
+Usage: ./day4-exercise2.py <sample1/t_data.ctab> <sample2/t_data.ctab>
+
+plot FPKM1 on x-axis and FPKM2 on y-axis
+"""
+
+import sys
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df1 = pd.read_csv( sys.argv[1], sep="\t", index_col="t_name" )
+df2 = pd.read_csv( sys.argv[2], sep="\t", index_col="t_name" )
+
+fpkm1 =df1.loc[:,"FPKM"]
+fpkm2 =df2.loc[:,"FPKM"]
+
+fit = np.polyfit(fpkm1, fpkm2,1)
+p = np.poly1d(fit)
+xplog = np.logspace(-3, 4, 50)
+
+#for log axis
+fig, ax = plt.subplots()
+ax.scatter(fpkm1, fpkm2, alpha=0.1)
+plt.plot(xplog, p(xplog), '-', color='m')
+plt.yscale("log")
+plt.xscale("log")
+ax.set_title("FPKM2 vs. FPKM1 Scatter Plot")
+plt.ylabel("log(FPKM2)")
+plt.xlabel("log(FPKM1)")
+plt.savefig("fpkmlog.png")
+plt.close()
+
+# #for non-log axis
+# fig, ax = plt.subplots()
+# ax.scatter(fpkm1, fpkm2, alpha=0.1)
+# plt.plot(xplog, p(xplog), '-', color='m')
+# # plt.yscale("log")
+# # plt.xscale("log")
+# ax.set_title("FPKM2 vs. FPKM1 Scatter Plot")
+# plt.ylabel("FPKM2")
+# plt.xlabel("FPKM1")
+# plt.savefig("fpkm.png")
+# plt.close()
+# ./day4-exercise2.py ~/data/results/stringtie/SRR072893/t_data.ctab ~/data/results/stringtie/SRR072915/t_data.ctab
